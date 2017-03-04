11b8339b341f42a9ceb5ea3cf7dc3a4617882c6d
Monica <monica.nickolai@gmail.com>
2017-03-04 11:21:27 +0000

    Added new users page and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -20,4 +20,10 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
     assert_select "title", "About | DJAC Online Process Lab"
   end
+  
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | DJAC Online Process Lab"
+  end
 end
